8a234092636833ca9fc9ddb78a558def29430a1c
MostafaAymanAwad <ayelshekh@gmail.com>
2022-12-08 01:18:59 +0200

    fix all

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -5,17 +5,34 @@ dotenv.config();
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
-
 export const config = {
-  // username: `${process.env.POSTGRES_USERNAME}`,
-  username: "postgres",
-  //password: `${process.env.POSTGRES_PASSWORD}`,
-  password: "12345",
-  //database: process.env.POSTGRES_DB,
-  database: "udagram",
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: String(process.env.POSTGRES_PASSWORD),
+  database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
-  dbport: Number(process.env.DB_PORT),
-  port: Number(process.env.PORT),
+ // port : Number(process.env.PORT),
+  //database_port : Number(process.env.DATABASE_PORT),
+  aws_region: process.env.AWS_REGION,
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
+
+/*export const config = {
+  username: `${process.env.POSTGRES_USERNAME}`,
+  //username: "postgres",
+  password: `${process.env.POSTGRES_PASSWORD}`,
+  //password: "12345",
+  database: process.env.POSTGRES_DB,
+  //database: "udagram",
+  host: process.env.POSTGRES_HOST,
+  //dbport: Number(process.env.DB_PORT),
+  //port: Number(process.env.PORT),
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
@@ -23,4 +40,4 @@ export const config = {
   jwt: {
     secret: process.env.JWT_SECRET,
   },
-};
+};*/

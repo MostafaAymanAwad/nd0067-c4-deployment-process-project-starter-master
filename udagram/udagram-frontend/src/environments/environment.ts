ea04ba48c0ebb5caeb42cf173fb300f7f5fafb95
MostafaAymanAwad <ayelshekh@gmail.com>
2022-12-08 01:47:55 +0200

    fix env link again

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: "http://udagram-env2.eba-uhnkusri.us-east-1.elasticbeanstalk.com/api/v0"
 };
 
 /*
